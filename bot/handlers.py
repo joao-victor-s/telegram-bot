6c0cd79ba7ce9e1da48fb088a80d1d582f74f63a
João Victor Santos <joaovictorsilvaesantos@gmail.com>
2025-04-30 18:01:58 -0300

    Add /matchs command to show last FURIA matches
    - Import get_furia_matches from scraping module
    - Implement matchs() handler to display last five matches with formatting
    - Register /matchs command in bot handler

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,8 +1,10 @@
 from telegram.ext import CommandHandler, MessageHandler, filters
-from bot.commands import unrecognized_command, start, news
+from bot.commands import unrecognized_command, start, news, matchs
 
 def register_handlers(application):
         application.add_handler(CommandHandler("start", start))
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, unrecognized_command))
         application.add_handler(CommandHandler("news", news))
+        application.add_handler(CommandHandler("matchs", matchs))
+
 
