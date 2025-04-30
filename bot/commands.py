33c2798df04fcbab7d64b1180290257f5aaad671
João Victor Santos <joaovictorsilvaesantos@gmail.com>
2025-04-30 18:01:58 -0300

    Add /matchs command to show last FURIA matches
    - Import get_furia_matches from scraping module
    - Implement matchs() handler to display last five matches with formatting
    - Register /matchs command in bot handler

--- bot/commands.py
+++ bot/commands.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from news.scraping import get_news
+from matches.get_matches import get_furia_matches
 
 from db.mongo_client import add_user
 
@@ -22,3 +23,18 @@ async def news(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for idx, news in enumerate(news_list, 1):
         message = f"{idx}. {news['title']}\n{news['snippet']}\n{news['link']}"
         await update.message.reply_text(message)
+
+async def matchs(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    furia_matches = get_furia_matches()
+    message_block = ""
+
+    for i, match in enumerate(furia_matches, 1):
+        message_block += (
+            f"🎯 *Partida {i}*\n"
+            f"📅 {match['date']}\n"
+            f"🏆 {match['tournament']} ({match['tier']} | {match['type']})\n"
+            f"🆚 *{match['team_1']}* vs *{match['team_2']}*\n"
+            f"🔢 *Placar:* {match['score']}\n\n"
+        )
+
+    await update.message.reply_text(message_block.strip(), parse_mode="Markdown")
